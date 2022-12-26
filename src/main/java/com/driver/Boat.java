6baceabeaaace30154a7e49ec16a4f761f49ae1c
Abhay Singh <abhaysingh07082000@gmail.com>
2022-12-26 18:25:58 +0530

    vehicle added

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -7,7 +7,15 @@ public class Boat implements WaterVehicle
 
    int capacity;
 
+ public Boat()
+ {
 
+ }
+ public Boat(String name,int capacity)
+ {
+     this.name=name;
+     this.capacity=capacity;
+ }
 
     @Override
     public String getVehicleName() {
